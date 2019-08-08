00f5664094d6394d2c004dfc698a317f2e3e0c51
Denis <happysmile05031@gmail.com>
2019-08-08 14:32:25 +0300

    Trd commit

--- src/main/java/com/exam/clonet/GreetingController.java
+++ src/main/java/com/exam/clonet/GreetingController.java
@@ -1,14 +1,21 @@
 package com.exam.clonet;
 
+import com.exam.clonet.domain.Message;
+import com.exam.clonet.repos.MessageRepos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepos messageRepos;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",required = false,defaultValue = "World")String name, Map<String, Object> model){
@@ -17,7 +24,29 @@ public class GreetingController {
     }
     @GetMapping
     public String main (Map <String,Object> model){
-        model.put("some", "Hello, LetsCode!");
+        Iterable<Message> messages= messageRepos.findAll();
+        model.put("messages", messages);
         return "main";
     }
+    @PostMapping ("add")
+    public String add(@RequestParam String text, @RequestParam String tag, Map <String,Object> model){
+        Message message=new Message(text,tag);
+        messageRepos.save(message);
+        Iterable<Message> messages=messageRepos.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping ("filter")
+    public String filter(@RequestParam String filter, Map<String,Object> model){
+        Iterable<Message> messages;
+        if(filter !=null && !filter.isEmpty()) {
+            messages = messageRepos.findByTag(filter);
+        }else {
+            messages = messageRepos.findAll();
+        }
+        model.put("messages" , messages);
+        return "main";
+    }
+
+
 }
--- src/main/java/com/exam/clonet/repos/MessageRepos.java
+++ src/main/java/com/exam/clonet/repos/MessageRepos.java
@@ -1,4 +1,13 @@
 package com.exam.clonet.repos;
 
-public interface MessageRepos {
+import com.exam.clonet.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepos extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
 }
+
